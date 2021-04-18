1071f55f08691a0d83f7393050890f3e685f4cb0
DANBAIWA HACKER <78322454+U-danbaiwa@users.noreply.github.com>
2021-04-18 19:56:01 +0100

    Update lookup-id.py

--- lookup-id.py
+++ lookup-id.py
@@ -72,8 +72,10 @@ try:
 		print(y+"%"*55)
 except Exception:
 	print(red+"\n\t\tSOMETHING WRONG!!!")
+os.system("chmod +x lookup-id.py")
 	sys.exit()
 print("")
+
 
 #ha=input("\tDID YOU WANT QUIT FROM THE TOOL?: ")
 #if ha=="yes" or ha=="y" or ha=="YES":
